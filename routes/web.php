8cefbb964d33227877e6671e4429d43f5ebad46d
adiatna sukmana <dyatna.id@gmail.com>
2022-04-30 16:23:23 +0700

    feat : add template layout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'loginAction']);
 Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', DashboardController::class);
+});
